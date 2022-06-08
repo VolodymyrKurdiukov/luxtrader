eba2575fc8b9b166a69588042a57b2a14c284407
Volodymyr <Volodymyr.kurdiukov@gmail.com>
2022-06-08 15:53:03 +0300

    header+main_slider(full)

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,5 +5,53 @@ $(document).ready(function () {
 		$('.header__burger,.header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
+	//==================================================================================================
+
+	//slider
+
+	$('.slider__row').slick({
+		// infinite: true,
+		// autoplay: true,
+		// autoplaySpeed: 5000,
+		arrows: true,
+		adaptiveHeight: true,
+		speed: 1200,
+		nextArrow: document.querySelector('.control__arrow-r'),
+		prevArrow: document.querySelector('.control__arrow-l')
+	});
 });
+//==================================================================================================
+
+//Выпадающее меню
+
+let user_icon = document.querySelector('.user__icon');
+user_icon.addEventListener("click", function (e) {
+	let user_menu = document.querySelector('.user__list');
+	user_menu.classList.toggle('active');
+});
+//==================================================================================================
+
+//клик вне области
+
+document.documentElement.addEventListener("click", function (e) {
+	if (!e.target.closest('.user')) {
+		let user_menu = document.querySelector('.user__list');
+		user_menu.classList.remove('active')
+	}
+});
+//==================================================================================================
+
+//ibg
+
+function ibg() {
+
+	let ibg = document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if (ibg[i].querySelector('img')) {
+			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+		}
+	}
+}
+
+ibg();
 //==================================================================================================
